c75922da0ece149235bd740dd292ce2798da2fc9
Jason <JasonBee@163.com>
2025-03-13 12:01:26 +0800

    add webrtc.framework

--- rtc_ios/ViewController.m
+++ rtc_ios/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <WebRTC/RTCPeerConnection.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    RTCPeerConnection *peer = [RTCPeerConnection init];
+    
 }
 
 
